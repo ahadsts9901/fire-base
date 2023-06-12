407507b14c9e7f62d4963ae2cdb8e8d6c6bc4dfa
ahadsts9901 <ahadsts990@gmail.com>
2023-06-12 13:06:39 -0700

    output done

--- main.js
+++ main.js
@@ -18,52 +18,6 @@ const db = firebase.firestore();
 async function makeCard(event) {
     event.preventDefault();
 
-    let container = document.querySelector(".mainContainer");
-    container.style.display = "flex";
-    let name = document.querySelector("#name");
-    let father = document.querySelector("#fatherName");
-    let age = document.querySelector("#age");
-    let rollNo = document.querySelector("#rollNo");
-
-    let card = document.createElement("div");
-    card.className += " column card";
-    container.appendChild(card);
-
-    let heading = document.createElement("h3");
-    heading.className = "pink";
-    heading.textContent = name.value;
-    card.appendChild(heading);
-
-    let ul = document.createElement("ul");
-    card.appendChild(ul);
-
-    let nameLi = document.createElement("li");
-    nameLi.className += " row-left";
-    nameLi.textContent = `Name : ${name.value}`;
-    ul.appendChild(nameLi);
-
-    let fatherLi = document.createElement("li");
-    fatherLi.className += " row-left";
-    fatherLi.textContent = `Father Name : ${father.value}`;
-    ul.appendChild(fatherLi);
-
-    let ageLi = document.createElement("li");
-    ageLi.className += " row-left";
-    ageLi.textContent = `Age : ${age.value}`;
-    ul.appendChild(ageLi);
-
-    let rollNoLi = document.createElement("li");
-    rollNoLi.className += " row-left";
-    rollNoLi.textContent = `SMIT Roll No : ${rollNo.value}`;
-    ul.appendChild(rollNoLi);
-
-    let para = document.createElement("p");
-    para.className += " center";
-    para.style.fontSize = "0.8em";
-    para.style.marginTop = "0.5em";
-    para.innerHTML = "Regards! <i>Muhammad Ahad&copy;</i>";
-    card.appendChild(para);
-
     let nameDB = document.getElementById('name').value
     let fatherDB = document.getElementById('fatherName').value
     let ageDB = document.getElementById('age').value
@@ -71,21 +25,74 @@ async function makeCard(event) {
 
     try {
         const docRef = await db.collection("users").add({
-            nameDB: nameDB,
-            fatherDB: fatherDB,
-            ageDB: ageDB,
-            rollNoDB: rollNoDB
+            name: nameDB,
+            father: fatherDB,
+            age: ageDB,
+            rollNo: rollNoDB
         });
 
         console.log("Document added successfully. ID:", docRef.id);
 
-        // Clearing input fields after successful submission
-        name.value = "";
-        father.value = "";
-        age.value = "";
-        rollNo.value = "";
     } catch (error) {
         console.error("Error adding document: ", error);
     }
 
+    let querySnapshot = await db.collection("users").get();
+    let container = document.querySelector(".mainContainer");
+    container.style.display = "flex";
+    // container.innerHTML = "";
+
+    querySnapshot.forEach((doc) => {
+        const data = doc.data();
+
+        let name = document.querySelector("#name");
+        let father = document.querySelector("#fatherName");
+        let age = document.querySelector("#age");
+        let rollNo = document.querySelector("#rollNo");
+
+        let card = document.createElement("div");
+        card.className += " column card";
+        container.appendChild(card);
+
+        let heading = document.createElement("h3");
+        heading.className = "pink";
+        heading.textContent = data.name;
+        card.appendChild(heading);
+
+        let ul = document.createElement("ul");
+        card.appendChild(ul);
+
+        let nameLi = document.createElement("li");
+        nameLi.className += " row-left";
+        nameLi.textContent = `Name : ${data.name}`;
+        ul.appendChild(nameLi);
+
+        let fatherLi = document.createElement("li");
+        fatherLi.className += " row-left";
+        fatherLi.textContent = `Father : ${data.father}`;
+        ul.appendChild(fatherLi);
+
+        let ageLi = document.createElement("li");
+        ageLi.className += " row-left";
+        ageLi.textContent = `Age : ${data.age}`;
+        ul.appendChild(ageLi);
+
+        let rollNoLi = document.createElement("li");
+        rollNoLi.className += " row-left";
+        rollNoLi.textContent = `Roll No : ${data.rollNo}`;
+        ul.appendChild(rollNoLi);
+
+        let para = document.createElement("p");
+        para.className += " center";
+        para.style.fontSize = "0.8em";
+        para.style.marginTop = "0.5em";
+        para.innerHTML = "Regards! <i>Muhammad Ahad&copy;</i>";
+        card.appendChild(para);
+
+        name.value = ""
+        father.value = ""
+        age.value = ""
+        rollNo.value = ""
+
+    })
 }
