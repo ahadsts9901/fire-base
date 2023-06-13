e074a8f98cdcd549be60ff5cd38b95610f947cd4
ahadsts9901 <ahadsts990@gmail.com>
2023-06-13 12:31:55 -0700

    delete edit remaining

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
+//sweet alert
+
+let timerInterval
+Swal.fire({
+    title: 'Loading...',
+    html: 'I will close in <b></b> milliseconds.',
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: () => {
+        Swal.showLoading()
+        const b = Swal.getHtmlContainer().querySelector('b')
+        timerInterval = setInterval(() => {
+            b.textContent = Swal.getTimerLeft()
+        }, 100)
+    },
+    willClose: () => {
+        clearInterval(timerInterval)
+    }
+}).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+        // console.log('I was closed by the timer')
+    }
+})
+
+// code start
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyD0dJrEkEoq9hpWmjFTq4NI1bWwpgPJ40E",
     authDomain: "simple-database-b15ab.firebaseapp.com",
     //   databaseURL: "https://simple-database-b15ab-default-rtdb.firebaseio.com",
@@ -9,90 +36,156 @@ const firebaseConfig = {
     messagingSenderId: "374723832608",
     appId: "1:374723832608:web:1fbfe6a876a1107a1b0545",
     //   measurementId: "G-0N19V5LFCN"
-}
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+var db = firebase.firestore();
 
-async function makeCard(event) {
+function makeCard(event) {
     event.preventDefault();
 
-    let nameDB = document.getElementById('name').value
-    let fatherDB = document.getElementById('fatherName').value
-    let ageDB = document.getElementById('age').value
-    let rollNoDB = document.getElementById('rollNo').value
+    var nameDB = document.getElementById("name").value;
+    var fatherDB = document.getElementById("fatherName").value;
+    var ageDB = document.getElementById("age").value;
+    var rollNoDB = document.getElementById("rollNo").value;
+    var timestamp = firebase.firestore.Timestamp.now().toMillis();
 
-    try {
-        const docRef = await db.collection("users").add({
+    db.collection("users")
+        .add({
             name: nameDB,
             father: fatherDB,
             age: ageDB,
-            rollNo: rollNoDB
+            rollNo: rollNoDB,
+            timestamp: timestamp
+        })
+        .then(function(docRef) {
+            //sweet alert
+
+            let timerInterval
+            Swal.fire({
+                title: 'Adding...',
+                html: 'I will close in <b></b> milliseconds.',
+                timer: 1000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                        b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval)
+                }
+            }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    // console.log('I was closed by the timer')
+                }
+            })
+            console.log("Document added successfully. ID:", docRef.id);
+            renderCards(); // Call renderCards after adding the document
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
         });
 
-        console.log("Document added successfully. ID:", docRef.id);
-
-    } catch (error) {
-        console.error("Error adding document: ", error);
-    }
-
-    let querySnapshot = await db.collection("users").get();
-    let container = document.querySelector(".mainContainer");
-    container.style.display = "flex";
-    // container.innerHTML = "";
-
-    querySnapshot.forEach((doc) => {
-        const data = doc.data();
-
-        let name = document.querySelector("#name");
-        let father = document.querySelector("#fatherName");
-        let age = document.querySelector("#age");
-        let rollNo = document.querySelector("#rollNo");
-
-        let card = document.createElement("div");
-        card.className += " column card";
-        container.appendChild(card);
-
-        let heading = document.createElement("h3");
-        heading.className = "pink";
-        heading.textContent = data.name;
-        card.appendChild(heading);
-
-        let ul = document.createElement("ul");
-        card.appendChild(ul);
-
-        let nameLi = document.createElement("li");
-        nameLi.className += " row-left";
-        nameLi.textContent = `Name : ${data.name}`;
-        ul.appendChild(nameLi);
-
-        let fatherLi = document.createElement("li");
-        fatherLi.className += " row-left";
-        fatherLi.textContent = `Father : ${data.father}`;
-        ul.appendChild(fatherLi);
-
-        let ageLi = document.createElement("li");
-        ageLi.className += " row-left";
-        ageLi.textContent = `Age : ${data.age}`;
-        ul.appendChild(ageLi);
-
-        let rollNoLi = document.createElement("li");
-        rollNoLi.className += " row-left";
-        rollNoLi.textContent = `Roll No : ${data.rollNo}`;
-        ul.appendChild(rollNoLi);
-
-        let para = document.createElement("p");
-        para.className += " center";
-        para.style.fontSize = "0.8em";
-        para.style.marginTop = "0.5em";
-        para.innerHTML = "Regards! <i>Muhammad Ahad&copy;</i>";
-        card.appendChild(para);
-
-        name.value = ""
-        father.value = ""
-        age.value = ""
-        rollNo.value = ""
-
-    })
+    document.getElementById("name").value = ""
+    document.getElementById("fatherName").value = ""
+    document.getElementById("age").value = ""
+    document.getElementById("rollNo").value = ""
 }
+
+
+function renderCards() {
+    var container = document.querySelector(".mainContainer");
+    container.style.display = "flex";
+    container.innerHTML = "";
+
+    db.collection("users")
+        .orderBy("timestamp", "asc") //sort by time
+        .get()
+        .then(function(querySnapshot) {
+            if (querySnapshot.size === 0) {
+                container.innerHTML = "<div class='blue'>No Users found</div>";
+            } else {
+                querySnapshot.forEach(function(doc) {
+                    var data = doc.data();
+
+                    var card = document.createElement("div");
+                    card.className = "column card";
+                    container.appendChild(card);
+
+                    var heading = document.createElement("h3");
+                    heading.className = "pink";
+                    heading.textContent = data.name;
+                    card.appendChild(heading);
+
+                    var ul = document.createElement("ul");
+                    card.appendChild(ul);
+
+                    var nameLi = document.createElement("li");
+                    nameLi.className = "row-left";
+                    nameLi.textContent = "Name: " + data.name;
+                    ul.appendChild(nameLi);
+
+                    var fatherLi = document.createElement("li");
+                    fatherLi.className = "row-left";
+                    fatherLi.textContent = "Father: " + data.father;
+                    ul.appendChild(fatherLi);
+
+                    var ageLi = document.createElement("li");
+                    ageLi.className = "row-left";
+                    ageLi.textContent = "Age: " + data.age;
+                    ul.appendChild(ageLi);
+
+                    var rollNoLi = document.createElement("li");
+                    rollNoLi.className = "row-left";
+                    rollNoLi.textContent = "Roll No: " + data.rollNo;
+                    ul.appendChild(rollNoLi);
+
+                    var para = document.createElement("div");
+                    para.className = "row para-row";
+                    para.style.fontSize = "0.8em";
+                    para.style.marginTop = "0.5em";
+                    card.appendChild(para);
+
+                    var description = document.createElement("p")
+                        // description.className += " center"
+                    description.innerHTML = "Regards! <i>Muhammad Ahad&copy;</i>";
+                    para.appendChild(description)
+
+                    var edit = document.createElement("i")
+                    edit.className += " bi bi-pencil-fill buttons"
+                    edit.addEventListener("click", editDoc)
+                    para.appendChild(edit)
+
+                    var del = document.createElement("i")
+                    del.className += " bi bi-trash-fill buttons"
+                    del.addEventListener("click", delDoc)
+                    para.appendChild(del)
+
+                    function delDoc(event) {
+                        event.preventDefault()
+                        console.log("delete function detected")
+                    }
+
+                    function editDoc(event) {
+                        event.preventDefault()
+                        console.log("edit function detected")
+                    }
+
+                });
+            }
+        })
+        .catch(function(error) {
+            console.log("Error getting documents: ", error);
+        });
+}
+
+document.addEventListener("readystatechange", function() {
+    if (document.readyState === "complete") {
+        renderCards();
+    }
+});
